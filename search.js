e2cf724d582b710cc4a16783b30ce36a1ddd50de
Raiyan Islam <157055271+Redappleee@users.noreply.github.com>
2024-10-07 01:23:09 +0530

    Update search.js

--- search.js
+++ search.js
@@ -1,3 +1,21 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const leftGate = document.getElementById("leftGate");
+    const rightGate = document.getElementById("rightGate");
+    const content = document.getElementById("content");
+  
+    // Delay for 0.5s to allow page load, then trigger animation
+    setTimeout(() => {
+      leftGate.classList.add("open");
+      rightGate.classList.add("open");
+  
+      // Once the gates are open, show the content
+      setTimeout(() => {
+        content.style.opacity = "1";
+      }, 2000); // Matches the duration of gate opening (2 seconds)
+    }, 500);
+  });
+
+
 const inputElement = document.querySelector("#search-input");
 const search_icon = document.querySelector("#search-close-icon");
 const sort_wrapper = document.querySelector(".sort-wrapper");
